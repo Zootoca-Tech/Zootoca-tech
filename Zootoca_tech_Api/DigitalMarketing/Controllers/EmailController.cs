6625bf62ecc297909f54499f5d5fad177919ee04
AKASH <109033257+AKASH-k-s63@users.noreply.github.com>
2024-02-05 11:22:09 +0530

    Api initial commit with mail service

---
+++ Zootoca_tech_Api/DigitalMarketing/Controllers/EmailController.cs
@@ -0,0 +1,46 @@
+﻿using DigitalMarketing.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Net;
+using System.Net.Mail;
+
+namespace DigitalMarketing.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmailController : Controller
+    {
+        [HttpPost("Sendmail")]
+        public IActionResult SendEmail(EmailModel model)
+        {
+            try
+            {
+                
+                MailMessage message = new MailMessage();
+                message.From = new MailAddress(model.From);
+                message.To.Add(model.To);
+                message.Subject = model.Subject;
+                message.Body = model.Body;
+                message.IsBodyHtml = true;
+
+                // Configure the SMTP client
+                SmtpClient smtpClient = new SmtpClient("smtp.gmail.com");
+                smtpClient.Port = 587; 
+                smtpClient.EnableSsl = true; 
+                smtpClient.Credentials = new NetworkCredential("akashks6341@gmail.com", "jdqt afoj cihq remp");
+                smtpClient.Credentials = new NetworkCredential("user@gmail.com", "Secret pwd");
+
+                // Send the email
+                smtpClient.Send(message);
+
+                return Ok("Email sent successfully");
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(new { Message = ex.Message.ToString() });
+            }
+        }
+    }
+
+
+}
