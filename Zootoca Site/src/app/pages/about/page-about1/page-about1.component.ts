b7f23fc37d3bfe0a549fd3c3f6d2537ea7d3efac
abishake <abishakesn@gmail.com>
2024-02-21 15:07:49 +0530

    about us animated text

--- Zootoca Site/src/app/pages/about/page-about1/page-about1.component.ts
+++ Zootoca Site/src/app/pages/about/page-about1/page-about1.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-page-about1',
@@ -7,14 +7,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PageAbout1Component implements OnInit {
 
+  isTextVisible: boolean = false;
+
   constructor() { }
 
+  @HostListener('window:scroll', ['$event'])
+  onScroll() {
+    const scrollPosition = window.scrollY || document.documentElement.scrollTop || document.body.scrollTop || 0;
+    const textElement = document.querySelector('.animi');
+
+    if (textElement) {
+      const elementPosition = textElement.getBoundingClientRect().top;
+      const windowHeight = window.innerHeight;
+
+      if (elementPosition < windowHeight * 0.95) {
+        this.isTextVisible = true;
+      } else {
+        this.isTextVisible = false; // Reset to false if scrolled out of view
+      }
+    }
+  }
+  
   ngOnInit(): void {
   }
-
+  
   banner = {
      //image: "assets/images/banner/5.jpg",
-     image: 'https://aws-in-eventphotos.s3.ap-south-1.amazonaws.com/BannerImages/About+Us.jpg',
+    image: 'https://aws-in-eventphotos.s3.ap-south-1.amazonaws.com/BannerImages/About+Us.jpg',
     title: "About Company",
     description: "The essence of interior design will always be about people and how they live. It is about the realities of what makes for an attractive, civilized.",
     page: "About 1"
@@ -28,8 +47,8 @@ export class PageAbout1Component implements OnInit {
   about = {
     image:"assets/new_images/Aboutimage.jpg",
     //image: "assets/images/background/bg5.jpg",
-    title: "Our floors are designed to last a lifetime",
-    description: "Since 1999, we have been providing great flooring solutions and customer service for homeowners and commercial clients. among flooring materials, none is more elegant and luxurious than natural stone."
+    title: "Get to know us, get to work with us",
+    description: "We are an integrated digital marketing company,who channelize client’s budget in online & offline campaigns or building your brand image for ensuring better future. we are a full service digital media company helping global brands communicate better with your customers.we help businesses achieve their goals through our outcome-oriented and data-driven digital marketing strategies.<br><br>we believe that all great ideas are born out of deep-rooted strategic thinking,By diving into a brand’s purpose,we create measurable and successful ideas that result in creative works that will work.<br><br>we arrive at a plan to achieve a goal while being ridiculous in expectation but feasible in execution.Trust us,we’ll leave you shocked & surprised.<br><br>In a world of monotonous marketing we’re the rebels,the misfits and the creative masterminds who dare to think outside the box.Say goodbye to the ordinary and Let’s dive into the EXTRAORDINARY with our ZOOTOCA."
   }
 
   // whatwedo = {
@@ -139,92 +158,92 @@ export class PageAbout1Component implements OnInit {
     ]
   }
 
-  // testimonials = {
-  //   title: "Testimonial",
-  //   testimonials: [
-  //     {
-  //       image: "assets/images/testimonials/pic1.jpg",
-  //       role: "Architect",
-  //       name: "Rosalina D. William",
-  //       quotes: "Great theme, just what we were looking for. Easy to install, easy to navigate. Well documented. Really enjoyed the support."
-  //     },
-  //     {
-  //       image: "assets/images/testimonials/pic2.jpg",
-  //       role: "Architect",
-  //       name: "Mitchal Jhon",
-  //       quotes: "Amazing fast and reliable customer support! The team of willing to go mile for customer service! Thanks!"
-  //     },
-  //     {
-  //       image: "assets/images/testimonials/pic3.jpg",
-  //       role: "Interior designer",
-  //       name: "Barney Smith",
-  //       quotes: "Great theme, just what we were looking for. Easy to install, easy to navigate. Well documented. Really enjoyed the support."
-  //     },
-  //     {
-  //       image: "assets/images/testimonials/pic4.jpg",
-  //       role: "Architect",
-  //       name: "Rosalina D. William",
-  //       quotes: "Amazing fast and reliable customer support! The team of willing to go mile for customer service! Thanks!"
-  //     }
-  //   ]
-  // }
   testimonials = {
-    experience: {
-      title: "Infographic",
-      items: [
-        {
-          count: "24",
-          title: "Years of Experience"
-        },
-        {
-          count: "340",
-          title: "Project Taken"
-        },
-        {
-          count: "86",
-          title: "Awards Won"
-        },
-        {
-          count: "36",
-          title: "Twitter Followers"
-        }
-      ]
-    },
+    title: "Testimonial",
+    testimonials: [
+      {
+        image: "assets/images/testimonials/pic1.jpg",
+        role: "Architect",
+        name: "Rosalina D. William",
+        quotes: "Great theme, just what we were looking for. Easy to install, easy to navigate. Well documented. Really enjoyed the support."
+      },
+      {
+        image: "assets/images/testimonials/pic2.jpg",
+        role: "Architect",
+        name: "Mitchal Jhon",
+        quotes: "Amazing fast and reliable customer support! The team of willing to go mile for customer service! Thanks!"
+      },
+      {
+        image: "assets/images/testimonials/pic3.jpg",
+        role: "Interior designer",
+        name: "Barney Smith",
+        quotes: "Great theme, just what we were looking for. Easy to install, easy to navigate. Well documented. Really enjoyed the support."
+      },
+      {
+        image: "assets/images/testimonials/pic4.jpg",
+        role: "Architect",
+        name: "Rosalina D. William",
+        quotes: "Amazing fast and reliable customer support! The team of willing to go mile for customer service! Thanks!"
+      }
+    ]
+  }
+//   testimonials = {
+//     experience: {
+//       title: "Infographic",
+//       items: [
+//         {
+//           count: "24",
+//           title: "Years of Experience"
+//         },
+//         {
+//           count: "340",
+//           title: "Project Taken"
+//         },
+//         {
+//           count: "86",
+//           title: "Awards Won"
+//         },
+//         {
+//           count: "36",
+//           title: "Twitter Followers"
+//         }
+//       ]
+//     },
     
-    testimonials: {
-      title: "Testimonial",
-      content: [
-        {
-          image: "assets/images/testimonials/ceo1.jpeg",
-          name: "Rosalina D. William",
-          role: "Architect",
-          quote: "“Amazing fast and reliable customer support! The team of developers are willing to go mile for customer service! Thanks! which was created for the bliss of souls like mine. I am so happy, my dear friend, so absorbed in the exquisite sense of me.”"
-        },
-        {
-          image: "assets/images/testimonials/ceo2.png",
-          name: "Mitchal Jhon",
-          role: "Architect",
-          quote: "“Amazing fast and reliable customer support! The team of developers are willing to go mile for customer service! Thanks! which was created for the bliss of souls like mine. I am so happy, my dear friend, so absorbed in the exquisite sense of me.”"
-        },
-        {
-          image: "assets/images/testimonials/ceo3.jpeg",
-          name: "Barney Smith",
-          role: "Interior designer",
-          quote: "“Great theme, just what we were looking for. Easy to install, easy to navigate. Well documented. Really enjoyed the support. which was created for the bliss of souls like mine. I am so happy, my dear friend, so absorbed in the exquisite sense of me.”"
-        },
-        {
-          image: "assets/images/testimonials/ceo4.jpg",
-          name: "Rosalina D. William",
-          role: "Architect",
-          quote: "“Great theme, just what we were looking for. Easy to install, easy to navigate. Well documented. Really enjoyed the support. which was created for the bliss of souls like mine. I am so happy, my dear friend, so absorbed in the exquisite sense of me.”"
-        },
-        {
-          image: "assets/images/testimonials/ceo5.jpeg",
-          name: "Rosalina D. William",
-          role: "CEO, Intearch",
-          quote: "“Great theme, just what we were looking for. Easy to install, easy to navigate. Well documented. Really enjoyed the support. which was created for the bliss of souls like mine. I am so happy, my dear friend, so absorbed in the exquisite sense of me.”"
-        }
-      ]
-    }
-}
-}
+//     testimonials: {
+//       title: "Testimonial",
+//       content: [
+//         {
+//           image: "assets/images/testimonials/ceo1.jpeg",
+//           name: "Rosalina D. William",
+//           role: "Architect",
+//           quote: "“Amazing fast and reliable customer support! The team of developers are willing to go mile for customer service! Thanks! which was created for the bliss of souls like mine. I am so happy, my dear friend, so absorbed in the exquisite sense of me.”"
+//         },
+//         {
+//           image: "assets/images/testimonials/ceo2.png",
+//           name: "Mitchal Jhon",
+//           role: "Architect",
+//           quote: "“Amazing fast and reliable customer support! The team of developers are willing to go mile for customer service! Thanks! which was created for the bliss of souls like mine. I am so happy, my dear friend, so absorbed in the exquisite sense of me.”"
+//         },
+//         {
+//           image: "assets/images/testimonials/ceo3.jpeg",
+//           name: "Barney Smith",
+//           role: "Interior designer",
+//           quote: "“Great theme, just what we were looking for. Easy to install, easy to navigate. Well documented. Really enjoyed the support. which was created for the bliss of souls like mine. I am so happy, my dear friend, so absorbed in the exquisite sense of me.”"
+//         },
+//         {
+//           image: "assets/images/testimonials/ceo4.jpg",
+//           name: "Rosalina D. William",
+//           role: "Architect",
+//           quote: "“Great theme, just what we were looking for. Easy to install, easy to navigate. Well documented. Really enjoyed the support. which was created for the bliss of souls like mine. I am so happy, my dear friend, so absorbed in the exquisite sense of me.”"
+//         },
+//         {
+//           image: "assets/images/testimonials/ceo5.jpeg",
+//           name: "Rosalina D. William",
+//           role: "CEO, Intearch",
+//           quote: "“Great theme, just what we were looking for. Easy to install, easy to navigate. Well documented. Really enjoyed the support. which was created for the bliss of souls like mine. I am so happy, my dear friend, so absorbed in the exquisite sense of me.”"
+//         }
+//       ]
+//     }
+// }
+ }
