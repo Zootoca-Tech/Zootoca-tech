9fd410a93bdd9b0a8cc4f5c30631a007faecb479
Martin <martinroy6982@gmail.com>
2024-02-22 11:05:34 +0530

    show less btn added in all services in home page and form validation reset fixed

--- Zootoca Site/src/app/pages/other/page-contact-us/page-contact-us.component.ts
+++ Zootoca Site/src/app/pages/other/page-contact-us/page-contact-us.component.ts
@@ -3,14 +3,15 @@ import { AbstractControl, FormBuilder, FormGroup, Validators } from '@angular/fo
 import { Router } from '@angular/router';
 import { EmailService } from 'src/web-api-services/email-service.service';
 import { NotifyService } from 'src/web-api-services/notify.service';
+import { NgForm } from '@angular/forms';
 //import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 @Component({
   selector: 'app-page-contact-us',
   templateUrl: './page-contact-us.component.html',
   styleUrls: ['./page-contact-us.component.css']
 })
-export class PageContactUsComponent {
-  contactusform: FormGroup;
+export class PageContactUsComponent implements OnInit{
+  contactusform!: FormGroup;
   con: any;
   submitted: boolean = false;
   isLoading: boolean = false;
@@ -21,49 +22,44 @@ export class PageContactUsComponent {
 
     private Toastr: NotifyService,
     private _emailservice: EmailService) { 
-
-    this.contactusform = this._fb.group(
-      {
-        UserName:['',[Validators.required]],
-        from: ['',[Validators.required]],
-        to:['zootocatechnologies@gmail.com'],//Add to email address(hardcore)
-        Message:[''],
-        ContactNumber:['',[Validators.required]],
-        // Address:[''],
-        Result:[''],
-       //subject as service
-        Service:['0',[Validators.required]]
-      });
-
   }
-
-
 
   ngOnInit(): void {
+    this.contactusform = this._fb.group({
+      userName: ['', Validators.required],
+      emailAddress: ['', [Validators.required, Validators.email]],
+      phoneNumber: ['', Validators.required],
+      selectedService: ['0', Validators.required],
+      message: ['', Validators.required],
+    });
   }
-
-  get f(): { [key: string]: AbstractControl } {
+  
+  get formControls() {
     return this.contactusform.controls;
   }
-
   OnSubmit() {
     this.submitted = true;
+  
     if (this.contactusform.invalid) {
       return;
     }
-    this._emailservice.sendemail(this.contactusform.value).subscribe(data => {
-      console.log(data)
-      this.contactusform.reset();
-    }, error => {
-      if (error.status === 200)
-        this.Toastr.showSuccess(error.error.text, "Success");
-      if (error.status === 500)
-        this.Toastr.showError(error.error.text, "Error");
-    }
-    )
+  
+    this._emailservice.sendemail(this.contactusform.value).subscribe(
+      (data) => {
+        console.log(data);
+       
+      },
+      (error) => {
+        if (error.status === 200)
+          this.Toastr.showSuccess(error.error.text, 'Success');
+        if (error.status === 500)
+          this.Toastr.showError(error.error.text, 'Error');
+      }
+    );
     this.contactusform.reset();
+    this.submitted = false;
   }
-
+  
   //this is service list binded with html//
   servicelist = [
     {
@@ -117,8 +113,8 @@ export class PageContactUsComponent {
 
   banner = {
     image: "https://aws-in-eventphotos.s3.ap-south-1.amazonaws.com/BannerImages/contact+us.jpg",
-    title: "Contact Us Form",
-    description: "The essence of interior design will always be about people and how they live. It is about the realities of what makes for an attractive, civilized.",
+    title: "Contact Us",
+    description: "Connect with us through our Contact Us page to explore how our digital marketing expertise can elevate your online presence. We're here to answer your questions and discuss tailored solutions for your business.",
     page: "Contact Us"
   }
 
