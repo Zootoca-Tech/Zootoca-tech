7c556daa220082daabf7fcb3045a7a849bbd8bdc
Martin <martinroy6982@gmail.com>
2024-03-10 17:13:58 +0530

    fadeup anumation updated in All services of Home page

--- Zootoca Site/src/assets/cursorfunction/cursor.js
+++ Zootoca Site/src/assets/cursorfunction/cursor.js
@@ -15,3 +15,19 @@ function trail (e) {
 }
 
 addEventListener('mousemove', trail);
+
+// FadeUp Animation for Home page
+window.addEventListener('scroll', reveal);
+function reveal(){
+  var reveals = document.querySelectorAll('.reveal');
+  for (var i = 0; i < reveals.length; i++){
+    var win_height = window.innerHeight;
+    var reveal_top = reveals[i].getBoundingClientRect().top;
+    var reveal_point = 150;
+    if (reveal_top < win_height - reveal_point) {
+      reveals[i].classList.add('active');
+    } //else {
+      //reveals[i].classList.remove('active');
+    //}
+  }
+}
