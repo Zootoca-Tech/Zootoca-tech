160d7ed7c51b7e70a8c5af695853bbcd9521cb81
Martin <martinroy6982@gmail.com>
2024-03-12 12:00:26 +0530

    fade animation updated in home and contact page

--- Zootoca Site/src/assets/cursorfunction/cursor.js
+++ Zootoca Site/src/assets/cursorfunction/cursor.js
@@ -31,3 +31,19 @@ function reveal(){
     //}
   }
 }
+
+////////////////////////////
+window.addEventListener('scroll', Reveal);
+function Reveal(){
+  var reveals = document.querySelectorAll('.Reveal');
+  for (var i = 0; i < reveals.length; i++){
+    var win_height = window.innerHeight;
+    var reveal_top = reveals[i].getBoundingClientRect().top;
+    var reveal_point = 150;
+    if (reveal_top < win_height - reveal_point) {
+      reveals[i].classList.add('active');
+    } //else {
+      //reveals[i].classList.remove('active');
+    //}
+  }
+}
